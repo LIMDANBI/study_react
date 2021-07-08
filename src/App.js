d0dc5dab96bf27104f62474157e9f0f98436f02b
dan_bibibi <dlaeksql08222@gmail.com>
2021-07-08 21:15:57 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
 
+  const [text, setText] = useState('danbi');
   const onSubmit = () => {
     alert('submitted');
   };
@@ -14,13 +16,26 @@ function App() {
     console.log('key up');
   }
 
+  // let text = 'danbi';
+
+  const updateText = () => {
+    // text = 'lim';
+    if(text === 'danbi')
+      setText('lim');
+    else
+    setText('danbi');
+    console.log(text);  
+  }
+
+  // console.log(text);
+
   return (
     <div className="App">
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -32,6 +47,10 @@ function App() {
       </header> */}
       <input onKeyUp={onKeyUp} />
       <button onClick={onSubmit}>submit</button>
+      <br/> <br/>
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>
+
     </div>
   );
 }
