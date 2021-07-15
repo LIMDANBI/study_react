8d31e801da37cad5073c95476c1bd8259bb3d443
dan_bibibi <dlaeksql08222@gmail.com>
2021-07-15 19:39:54 +0900

    props (자식 component에 data 보내기)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import React, { useEffect, useState } from 'react';
 import Counter from './components/Counter';
 // Component 사용 => 반복 제거 가능 
 // component 구성 : import React, {} from 'react', 함수 작성 -> jsx 리턴, export default -> 다른 곳에서 import 해서 사용 
+// 다른 component를 import해서 사용하는 component = 부모 component 
+// Props : 부모 component에서 자식 component로 데이터를 보내주고 싶을 때 사용 !
 
 function App() {
-  // const [count, setCount] = useState(0);
 
-  // const increment = () => {
-  //   setCount(count + 1);
-  // };
-
+  const [buttonName, setButtonName] = useState('클릭');
+  const clickButton = () => {
+    setButtonName('click');
+  }
+  
   return (
     <div className="App">
       <h1> Danbi's Story </h1>
-      <Counter/><Counter/><Counter/>
+      <Counter click = "click1"/> {/* 부모애서 click(click1)이라는 props를 보냄 (string )*/}
+      <Counter click = {buttonName}/> {/* (state)*/}
+      <Counter />
+      <button onClick = {clickButton}>change</button>
     </div>
   );
 }
 
 export default App;
+// props (parent component -> child component)
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,21 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 // Component 사용 => 반복 제거 가능 
 // component 구성 : import React, {} from 'react', 함수 작성 -> jsx 리턴, export default -> 다른 곳에서 import 해서 사용 
 // counter관련 logic이 들어 있음 => App.js에서 import해서 사용
+// 사용되어지는 component = 자식 component 
 
-const Counter = () => {
+//props라는 object를 통해서 부모 component에서 보낸 props를 받아올 수 있음 (string 뿐만 아니라 state도 보낼 수 o)
+const Counter = (props) => {
     const [count, setCount] = useState(0);
 
     const increment = () => {
         setCount(count + 1);
     };
 
+    const clickString = props.click || 'Click'; // props.click이 존재하면 사용, 존재하지 않으면 Click(기본 값) 사용
+
     return (
-        <button onClick={increment}>Click {count} </button>
+        <button onClick={increment}>{clickString} {count} </button>
     );
 };
 
