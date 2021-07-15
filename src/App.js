aa28aa72821ee60d8e1d5542dc078bf5f44de0eb
dan_bibibi <dlaeksql08222@gmail.com>
2021-07-15 20:14:07 +0900

    conditional-rendering

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import Counter from './components/Counter';
-// Component 사용 => 반복 제거 가능 
-// component 구성 : import React, {} from 'react', 함수 작성 -> jsx 리턴, export default -> 다른 곳에서 import 해서 사용 
-// 다른 component를 import해서 사용하는 component = 부모 component 
-// Props : 부모 component에서 자식 component로 데이터를 보내주고 싶을 때 사용 !
+import { render } from '@testing-library/react';
 
 function App() {
 
-  const [buttonName, setButtonName] = useState('클릭');
-  const clickButton = () => {
-    setButtonName('click');
-  }
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+  useEffect(() => {
+    console.log(condition);
+  }, [condition])
+  // const renderCondition = condition ? <div>True</div> : <div>False</div>
+  const renderCondition = condition ? 'true' : 'false'
   
   return (
     <div className="App">
-      <h1> Danbi's Story </h1>
-      <Counter click = "click1"/> {/* 부모애서 click(click1)이라는 props를 보냄 (string )*/}
-      <Counter click = {buttonName}/> {/* (state)*/}
-      <Counter />
-      <button onClick = {clickButton}>change</button>
+      <h1> Developer </h1>
+      {/* { condition ? <div>True</div> : <div>False</div>} */}
+      {/* {renderCondition} */}
+      <div>
+        {renderCondition}
+      </div>
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
 
 export default App;
-// props (parent component -> child component)
