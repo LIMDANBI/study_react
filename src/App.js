b3bc8e8534e68e8203b893b29c2e69c27e891cb4
dan_bibibi <dlaeksql08222@gmail.com>
2021-07-19 20:45:38 +0900

    영화 List 추가 폼 만들기 1

--- src/App.js
+++ src/App.js
@@ -4,40 +4,53 @@ import Movie from './components/Movie';
 import { render } from '@testing-library/react';
 
 function App() {
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
 
-  const movies = [
-    { title: 'harry potter1', year: 2001 },
-    { title: 'harry potter2', year: 2002 },
-    { title: 'harry potter3', year: 2004 },
-    { title: 'harry potter4', year: 2005 },
-    { title: 'harry potter5', year: 2007 },
-  ];
-
+  // movies가 state가 아니기 때문에 '영화추가'를 눌러도 rendering이 되지 x 
+  // useEffect(() =>{
+  //   console.log('render');
+  // })
+  const [movies, setMovies] = useState(
+    [
+      { title: 'harry potter1', year: 2001 },
+      { title: 'harry potter2', year: 2002 },
+      { title: 'harry potter3', year: 2004 },
+      { title: 'harry potter4', year: 2005 },
+      { title: 'harry potter5', year: 2007 },
+    ]
+  );
   const renderMovies = movies.map(movie => {
     return (
-      <Movie movie={movie} key={movie.title}/> // component를 사용하기 때문에 component에 key를 넣어 줌
+      <Movie movie={movie} key={movie.title} /> // component를 사용하기 때문에 component에 key를 넣어 줌
     )
   });
+
+  const addMovie = (event) => {
+    event.preventDefault(); // form 사용 시 submit을 했을 때 page reloading 방지
+    // console.log(movieTitle, movieYear); // 출력 확인
+    // movies.push({
+    //   title: movieTitle,
+    //   year: movieYear
+    // })
+    setMovies([
+      ...movies,
+      {
+        title: movieTitle,
+        year: movieYear,
+      }
+    ])
+  };
 
   return (
     <div className="App">
       <h1> Movie list </h1>
+      <form onSubmit={addMovie}>
+        <input type="text" value={movieTitle} placeholder="영화제목" onChange={e => setMovieTitle(e.target.value)} /> <br />
+        <input type="text" value={movieYear} placeholder="개봉년도" onChange={e => setMovieYear(e.target.value)} /> <br />
+        <button type="submit">영화추가</button>
+      </form>
       {renderMovies}
-      {/* <div className="movie">
-        <div className="movie-title">{movies[0].title}</div>
-        <div className="movie-year">{movies[0].year}</div>
-      </div>
-
-      <div className="movie">
-        <div className="movie-title">{movies[1].title}</div>
-        <div className="movie-year">{movies[1].year}</div>
-      </div>
-
-      <div className="movie">
-        <div className="movie-title">{movies[2].title}</div>
-        <div className="movie-year">{movies[2].year}</div>
-      </div> */}
-
     </div>
   );
 }
