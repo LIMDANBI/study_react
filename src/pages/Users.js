78bba33d4ddc0a1e25034071e9f076a779e4886d
dan_bibibi <dlaeksql08222@gmail.com>
2021-08-06 19:35:18 +0900

    영화 웹사이트 : 유저 데이터 화면에 출력

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import UserList from '../components/UserList';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
-                console.log(response)
-             });
+                setUsers(response.data);
+            });
     }, []); // 컴포넌트가 처음 실행될 때 한번만 데이터를 가져옴
+
+    // 잘 들어갔는지 확인 , [users] -> users state가 변경될 때 마다 이부분 실행
+    // useEffect(() => {
+    //     console.log(users);
+    // }, [users])
+
     return (
-        <h1>Users</h1>
+        <>
+            <h1>Users</h1>
+            <UserList users = {users} />
+        </>
     )
 }
 export default Users;
