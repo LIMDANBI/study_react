7eb4036d7e391ff39a035a154b74f7d3fd40b1b8
danbibibi <dlaeksql08222@gmail.com>
2021-08-21 15:56:14 +0900

    영화 웹사이트 : 로딩 spinner 추가

---
+++ src/components/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div class="spinner-border" 
+            style= {{width: '3rem', height: '3rem'}} 
+            role="status"
+        xs>
+            <span class="visually-hidden">
+                Loading...
+            </span>
+        </div>
+    );
+}
+
+export default Spinner;
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import UserList from '../components/UserList';
+import Spinner from '../components/Spinner';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
                 setUsers(response.data);
+                setLoading(false);
             });
     }, []); // 컴포넌트가 처음 실행될 때 한번만 데이터를 가져옴
 
@@ -19,7 +23,7 @@ const Users = () => {
     return (
         <>
             <h1>Users</h1>
-            <UserList users = {users} />
+            {loading ? <Spinner /> : <UserList users={users} />}
         </>
     )
 }
