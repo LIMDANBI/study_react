0ac487e050ed305e96e226ac1f3879908b6724d9
danbibibi <dlaeksql08222@gmail.com>
2021-09-04 04:42:47 +0900

    영화 웹사이트: Hroutes 파일 만들기

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import routes from './routes';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useRouteMatch,
-  useParams
 } from "react-router-dom";
-import Users from "./pages/Users";
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import { render } from '@testing-library/react';
-
 
 function App() {
-
   return (
     <Router>
       <div className="App">
         <Navbar />
-
         <div className="container">
-          {/* switch => 순서 중요 !!!!!!! (또는) exact 속성을 넣어주면 완전히 똑같아야지만 매치가 됨 !!! ㄴ*/}
           <Switch>
-            {/* navbar 밑 변경 */}
-
-            {/* home */}
-            <Route path="/" exact>
-              <Home />
-            </Route>
-
-            {/* movies */}
-            <Route path="/movies">
-              <Movies />
-            </Route>
-
-            {/* users */}
-            <Route path="/users">
-              <Users />
-            </Route>
-
+            {routes.map(route => {
+              return(
+                <Route 
+                  key = {route.path} 
+                  path = {route.path} 
+                  exact
+                > 
+                  <route.component />
+                </Route>
+              )
+            })}
           </Switch>
         </div>
-
       </div>
     </Router>
   );
 }
-
 export default App;
